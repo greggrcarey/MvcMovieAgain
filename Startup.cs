e66fc709ced68ceb5d3b30a16b39d1db03f0c46d
gcarey <gregg.carey@outlook.com>
2020-01-07 10:07:30 -0600

    Model, DB context, scaffold, inital migration

--- Startup.cs
+++ Startup.cs
@@ -5,25 +5,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
+
 
 namespace MvcMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment {get;}
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if(Environment.IsDevelopment()){
+                services.AddDbContext<MvcMovieContext>(options => 
+                options.UseSqlite(
+                    Configuration.GetConnectionString("MvcMovieContext")));
+            }
+            else{
+                services.AddDbContext<MvcMovieContext>(options => 
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("MvcMovieContext")));
+            }
+
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
